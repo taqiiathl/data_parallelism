d3dfc435f417f27aac0146c94524e0ae0469bfad
Taqii Athallah <taqiiathallah06@gmail.com>
2026-05-12 11:11:40 +0700

    fix code data parallelism

--- dataparallel.py
+++ dataparallel.py
@@ -1,18 +1,21 @@
 from multiprocessing import Pool
 import os
+import time
 
-# fungsi yang akan dijalankan paralel
 def process_data(data):
     pid = os.getpid()
+
+    # simulasi proses berat
+    time.sleep(1)
+
     result = data * data
     print(f"Process {pid} memproses data {data} -> hasil {result}")
+
     return result
 
 if __name__ == "__main__":
-    # data berbeda untuk tiap proses
     numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
-    # membuat 4 proses paralel
     with Pool(4) as p:
         results = p.map(process_data, numbers)
 
